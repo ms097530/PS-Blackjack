015494787728335eba982a9b3b152278bd2dcde5
Michael Schultz <mikewschultz9029@gmail.com>
2023-02-28 11:53:54 -0500

    redid file structure with modules and created tests for Card class

---
+++ card.test.js
@@ -0,0 +1,44 @@
+import { Card } from "./card.js"
+
+// make sure only proper cards can be created
+let myCard
+try
+{
+    myCard = new Card('queen', 'club')
+}
+catch (e)
+{
+    console.log(e.message)
+}
+console.log(myCard)
+
+// create deck and shuffle it
+let myDeck = Card.createDeck()
+console.log(myDeck)
+Card.shuffle(myDeck)
+
+console.log('deck is complete initially: ', getIsDeckProper(myDeck))
+console.log('deck is complete after shuffle: ', getIsDeckProper(myDeck))
+
+function getIsDeckProper(deck)
+{
+    let card_suits = Card.getSuits()
+    let card_values = Card.getValues()
+
+    // console.log(card_suits)
+    // console.log(card_values)
+    // console.log(deck)
+
+    for (let suit of card_suits)
+    {
+        for (let value of card_values)
+        {
+
+            let numMatches = deck.filter(card => card.getSuit() === suit && card.getValue() === value).length
+            if (numMatches !== 1)
+                return false
+        }
+    }
+
+    return true
+}
