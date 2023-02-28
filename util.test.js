80c90924fea54a12e14354b16bfc763db0bf3520
Michael Schultz <mikewschultz9029@gmail.com>
2023-02-28 14:49:57 -0500

    added first test

--- util.test.js
+++ util.test.js
@@ -3,3 +3,19 @@ import { Dealer } from "./dealer.js";
 import { getBlackjackHandTotal } from "./util.js";
 
 console.log(`-------------- UTIL TESTS --------------`)
+
+let testPlayer = new Player('Test Testerson', 420, 'tzeentch')
+let testDealer = new Dealer()
+
+testDealer.shuffle()
+
+testDealer.hitSelf()
+testDealer.hitSelf()
+
+testPlayer.hit(testDealer)
+testPlayer.hit(testDealer)
+
+console.log('hand total of dealer:')
+getBlackjackHandTotal(testDealer)
+console.log('hand total of player')
+getBlackjackHandTotal(testPlayer)
