08849874f5ebd73d7e558d787360dc6917f7e8da
Michael Schultz <mikewschultz9029@gmail.com>
2023-03-03 14:35:05 -0500

    updating name and chip amount

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ import { NPC } from "./npc.js";
 import { Dealer } from "./dealer.js";
 import { compareHands, getBlackjackHandTotal } from "./util.js";
 
-let dialogs = document.querySelectorAll('#starting-dialogs dialog')
-let dialogBtns = document.querySelectorAll('#starting-dialogs dialog button')
-let dialogInput = []
+const dialogs = document.querySelectorAll('#starting-dialogs dialog')
+const dialogBtns = document.querySelectorAll('#starting-dialogs dialog button')
+const dialogInput = []
+
+const playerChipsArea = document.querySelector('#players-area .chips .amount')
 
 // show first dialog
 // setTimeout(() =>
@@ -51,9 +53,12 @@ for (let i = 0; i < dialogBtns.length; ++i)
             console.log(dialogInput)
             let gameArea = document.getElementById('game-area')
             let gameBg = document.getElementById('game-bg')
+            let playerNameArea = document.querySelector('#players-area .score-name')
+            playerNameArea.textContent = dialogInput[0]
             gameArea.classList.remove('opacity-0')
             gameBg.classList.add('blur')
-            // playGame()
+
+            playGame()
         })
     }
 }
@@ -88,122 +93,123 @@ function playGame()
 
     // initialize dealer and player(s)
     const dealer = new Dealer()
-    const player = new Player('Test Testerson II', STARTING_CHIPS, 'khorne')
+    const player = new Player(dialogInput[0], STARTING_CHIPS, 'khorne')
+    playerChipsArea.textContent = player.getNumChips()
     let isStillPlaying = true
     // start game loop
-    while (player.getNumChips() > 0 && isStillPlaying)
-    {
-        // get player's bet and place it
-        let playerBet = prompt(`Place your bet (current chips: ${player.getNumChips()}):`)
-        player.bet(parseInt(playerBet))
+    // while (player.getNumChips() > 0 && isStillPlaying)
+    // {
+    //     // get player's bet and place it
+    //     let playerBet = prompt(`Place your bet (current chips: ${player.getNumChips()}):`)
+    //     player.bet(parseInt(playerBet))
 
-        // shuffle deck before dealing cards each round
-        dealer.shuffle()
+    //     // shuffle deck before dealing cards each round
+    //     dealer.shuffle()
 
-        // player(s) and dealer get two cards
-        player.hit(dealer)
-        player.hit(dealer)
+    //     // player(s) and dealer get two cards
+    //     player.hit(dealer)
+    //     player.hit(dealer)
 
-        dealer.hitSelf()
-        dealer.hitSelf()
+    //     dealer.hitSelf()
+    //     dealer.hitSelf()
 
-        console.log('Your hand:')
-        console.log(player.getHand())
-        console.log(getBlackjackHandTotal(player))
+    //     console.log('Your hand:')
+    //     console.log(player.getHand())
+    //     console.log(getBlackjackHandTotal(player))
 
-        // player decision time
-        let canStillHit = getBlackjackHandTotal(player) >= 21 ? false : true
-        while (canStillHit)
-        {
-            let answer = prompt(`Hit or stand? (current score: ${getBlackjackHandTotal(player)})`)
-            if (answer === 'hit')
-            {
-                player.hit(dealer)
-                console.log(`New total: ${getBlackjackHandTotal(player)}`)
-            }
-            else
-            {
-                canStillHit = false
-            }
+    //     // player decision time
+    //     let canStillHit = getBlackjackHandTotal(player) >= 21 ? false : true
+    //     while (canStillHit)
+    //     {
+    //         let answer = prompt(`Hit or stand? (current score: ${getBlackjackHandTotal(player)})`)
+    //         if (answer === 'hit')
+    //         {
+    //             player.hit(dealer)
+    //             console.log(`New total: ${getBlackjackHandTotal(player)}`)
+    //         }
+    //         else
+    //         {
+    //             canStillHit = false
+    //         }
 
-            if (getBlackjackHandTotal(player) >= 21)
-                canStillHit = false
-        }
-        console.log('Your hand after possibly hitting:')
-        console.log(player.getHand())
-        console.log(getBlackjackHandTotal(player))
+    //         if (getBlackjackHandTotal(player) >= 21)
+    //             canStillHit = false
+    //     }
+    //     console.log('Your hand after possibly hitting:')
+    //     console.log(player.getHand())
+    //     console.log(getBlackjackHandTotal(player))
 
-        // player phase over, finish dealer's hand
-        while (getBlackjackHandTotal(dealer) < 17)
-        {
-            dealer.hitSelf()
-        }
+    //     // player phase over, finish dealer's hand
+    //     while (getBlackjackHandTotal(dealer) < 17)
+    //     {
+    //         dealer.hitSelf()
+    //     }
 
-        // compare hands
-        console.log('Dealer hand after possibly hitting:')
-        console.log(dealer.getHand())
-        console.log(getBlackjackHandTotal(dealer))
+    //     // compare hands
+    //     console.log('Dealer hand after possibly hitting:')
+    //     console.log(dealer.getHand())
+    //     console.log(getBlackjackHandTotal(dealer))
 
-        // 0 = tie, 1 = player win, -1 = dealer win
-        let result = compareHands(player, dealer)
-        switch (result)
-        {
-            case -1:
-                {
-                    console.log('You lose, dealer wins.')
-                    player.adjustChips(-1)
-                    break
-                }
-            case 0:
-                {
-                    console.log('It\'s a draw!')
-                    break
-                }
-            case 1:
-                {
-                    console.log('You win! Collect your chips!')
-                    player.adjustChips(1)
-                    break
-                }
-            case 2:
-                {
-                    console.log('Blackjack win! Collect even more chips!')
-                    player.adjustChips(2)
-                    break
-                }
-            default:
-                {
-                    console.log('Something unexpected happened...')
-                    break
-                }
-        }
+    //     // 0 = tie, 1 = player win, -1 = dealer win
+    //     let result = compareHands(player, dealer)
+    //     switch (result)
+    //     {
+    //         case -1:
+    //             {
+    //                 console.log('You lose, dealer wins.')
+    //                 player.adjustChips(-1)
+    //                 break
+    //             }
+    //         case 0:
+    //             {
+    //                 console.log('It\'s a draw!')
+    //                 break
+    //             }
+    //         case 1:
+    //             {
+    //                 console.log('You win! Collect your chips!')
+    //                 player.adjustChips(1)
+    //                 break
+    //             }
+    //         case 2:
+    //             {
+    //                 console.log('Blackjack win! Collect even more chips!')
+    //                 player.adjustChips(2)
+    //                 break
+    //             }
+    //         default:
+    //             {
+    //                 console.log('Something unexpected happened...')
+    //                 break
+    //             }
+    //     }
 
-        // collect cards
-        dealer.collectCards(player)
+    //     // collect cards
+    //     dealer.collectCards(player)
 
-        console.log('Dealer\'s deck:')
-        console.log(dealer.getDeck())
-        console.log('Dealer\'s hand:')
-        console.log(dealer.getHand())
-        console.log('Player\'s hand:')
-        console.log(player.getHand())
+    //     console.log('Dealer\'s deck:')
+    //     console.log(dealer.getDeck())
+    //     console.log('Dealer\'s hand:')
+    //     console.log(dealer.getHand())
+    //     console.log('Player\'s hand:')
+    //     console.log(player.getHand())
 
-        console.log('Total chips: ', player.getNumChips())
+    //     console.log('Total chips: ', player.getNumChips())
 
-        // check if user CAN keep playing
-        // if yes, check if user WANTS to continue
-        if (player.getNumChips() > 0)
-        {
-            let keepPlaying = prompt('Continue playing? (Y or N)')
-            if (keepPlaying.toLowerCase() === 'n')
-                isStillPlaying = false
-        }
-        else
-        {
-            isStillPlaying = false
-        }
+    //     // check if user CAN keep playing
+    //     // if yes, check if user WANTS to continue
+    //     if (player.getNumChips() > 0)
+    //     {
+    //         let keepPlaying = prompt('Continue playing? (Y or N)')
+    //         if (keepPlaying.toLowerCase() === 'n')
+    //             isStillPlaying = false
+    //     }
+    //     else
+    //     {
+    //         isStillPlaying = false
+    //     }
 
-    }
+    // }
 
     console.log(`You walked away with ${player.getNumChips()} chips`)
 }
